41fa1bf26d86c9ca53a72728119ea3deeea0d5ad
Oliver Sundbäck <oliver.sundback93@gmail.com>
2016-04-06 15:59:09 +0200

    Användarklass med verifiering av uppgifter vid inloggning

---
+++ src/informatikb_system/Anvandare.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package informatikb_system;
+
+import java.util.ArrayList;
+import oru.inf.InfException;
+import oru.inf.InfDB;
+
+/**
+ *
+ * @author Oliver
+ */
+public class Anvandare {
+    private InfDB idb;
+    
+    public Anvandare()
+    {
+        try
+        {
+            String path = System.getProperty("user.dir"); //Hämtar user direcotry
+            idb = new InfDB(path + "/databas/DATABASE.FDB"); 
+            System.out.println("Uppkopplingen lyckades");
+        }
+        catch(InfException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public boolean valideraInloggning(String anvandarnamn, String losenord)
+    {
+        String sqlFraga = "SELECT ANSTALLD.LOSENORD "
+                + "FROM ANSTALLD WHERE ANSTALLD.ANVANDARNAMN = '" + anvandarnamn + "';";
+        String valideraLosenord = "";
+        try        {
+
+            valideraLosenord = idb.fetchSingle(sqlFraga);
+            if(valideraLosenord.equals(losenord))
+            {
+                return true;
+            }
+        }
+        catch(InfException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+    
+    
+}
